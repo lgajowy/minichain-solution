fa6df2c065457ce18f0c57c0d38993d1460ebfd8
Lukasz Gajowy <lukasz.gajowy@adverity.com>
2021-11-07 00:36:40 +0100

    Put the whole block as byte array to the Sha256 function

--- src/main/scala/com/lgajowy/minichain/algebras/HashCalculator.scala
+++ src/main/scala/com/lgajowy/minichain/algebras/HashCalculator.scala
@@ -1,8 +1,7 @@
 package com.lgajowy.minichain.algebras
 
 import cats.effect.kernel.Sync
-import com.lgajowy.minichain.domain.{ Block, Hash }
-import com.lgajowy.minichain.tools.BasePrimitives.Bytes
+import com.lgajowy.minichain.domain.{Block, Hash}
 import com.lgajowy.minichain.tools.Sha256
 
 trait HashCalculator[F[_]] {
@@ -13,13 +12,7 @@ object HashCalculator {
   def make[F[_]: Sync](): HashCalculator[F] = new HashCalculator[F] {
     override def calculate(block: Block): F[Hash] = {
       Sync[F].delay {
-        val hash: Bytes = Sha256(
-          Array(block.index.value.toByte),
-          block.parentHash.bytes,
-          block.transactions.flatMap(tx => tx.data.getBytes).toArray,
-          Array(block.nonce.value.toByte),
-          Array(block.miningTargetNumber.value.toByte)
-        )
+        val hash = Sha256(block.toString.getBytes())
         Hash(hash)
       }
     }
