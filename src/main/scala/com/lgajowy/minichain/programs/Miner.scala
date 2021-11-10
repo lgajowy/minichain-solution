8bd7677f67f47bb68771665e0ed6ff679c415e96
Lukasz Gajowy <lukasz.gajowy@adverity.com>
2021-11-10 02:54:28 +0100

    Add miner with its basic functionality tests
    The mining algorithm:
     - get a nonce
     - get nonce's bytes and append them to the previously computed blockTemplate's bytes
     - calculate hash from the bytes
     - see if such hash satisfies the mining condition (verification part)
     - do in loop until good nonce is found or the fiber is cancelled

--- src/main/scala/com/lgajowy/minichain/programs/Miner.scala
+++ src/main/scala/com/lgajowy/minichain/programs/Miner.scala
@@ -1,48 +1,51 @@
 package com.lgajowy.minichain.programs
 
-import com.lgajowy.minichain.domain.{ Block, Hash, MiningTarget, Transaction }
+import cats.effect.kernel.Async
+import cats.implicits._
+import com.lgajowy.minichain.algebras.{ BlockVerifier, HashProvider, NonceProvider }
+import com.lgajowy.minichain.base.BasePrimitives.Bytes
+import com.lgajowy.minichain.domain.MiningTarget.StdMiningTarget
+import com.lgajowy.minichain.domain._
+import com.lgajowy.minichain.effects.Race
+import com.lgajowy.minichain.ext.Serializer.serialize
 import com.lgajowy.minichain.ext.Sha256
 
-object Miner {
+final case class Miner[F[_]: Async: Race](
+  hashProvider: HashProvider[F],
+  nonces: NonceProvider[F],
+  blocks: BlockVerifier[F],
+  parallelism: Int
+) {
 
-  // Whoa! We actually mine the Genesis block.
-  // Normally, this is done by the system during bootstrapping
-  // and every other block is mined by a miner.
-  final val Genesis = Miner.mineNextBlock(
-    index = 0, // The very first block
-    parentHash = Hash(Sha256.ZeroHash), // Let's assume this is by definition for the Genesis block.
-    transactions = Seq(Transaction("Hello Blockchain, this is Genesis :)")),
-    MiningTarget.StdMiningTarget
-  )
+  def mine(blockTemplate: BlockTemplate): F[Block] = {
 
-  // And now let's implement the actual "proof-of-work"-style computation.
-  // Compare the parameters of this method with the fields of a Block and
-  // you'll see that the only thing missing here is the nonce. Here is why.
-  //
-  // Initially we have all the fixed elements a block:
-  //
-  //  - index,
-  //  - parentHash,
-  //  - transactions,
-  //  - miningTargetNumber
-  //
-  // and by varying the nonce we try to have a block hash that is below the
-  // given miningTargetNumber.
-  //
-  // NOTE Remember that the block hash can be transformed to an actual number,
-  //      so we can talk about hash and number interchangeably.
-  def mineNextBlock(
-    index: Int,
-    parentHash: Hash,
-    transactions: Seq[Transaction],
-    miningTargetNumber: MiningTarget
-  ): Block = {
-    // Solve this informal inequality for nonce:
-    //
-    //   Hash(block; nonce).toNumber < miningTargetNumber
-    //
-    // where Hash(block; nonce) is a function of nonce only, all the other block
-    // field values are just the given method arguments.
-    ???
+    val blockTemplateBytes: Bytes = serialize(blockTemplate)
+
+    def task(blockTemplateBytes: Bytes): F[Block] = {
+      val nonceCandidate: F[(Nonce, Boolean)] = for {
+        nonce <- nonces.getNextNonce()
+        nonceBytes = serialize(nonce)
+        blockBytes = blockTemplateBytes ++ nonceBytes
+        blockHash <- hashProvider.getHashDigest(blockBytes)
+        isVerified <- blocks.verify(blockHash, blockTemplate.miningTarget)
+      } yield (nonce, isVerified)
+
+      nonceCandidate
+        .iterateUntil { case (_, isValid) => isValid }
+        .map { case (nonce, _) => blockTemplate.toBlock(nonce) }
+    }
+
+    Race[F].race(parallelism, task(blockTemplateBytes))
+  }
+
+  def mineGenesis(): F[Block] = {
+    val genesisTemplate = BlockTemplate(
+      Index(0),
+      Hash(Sha256.ZeroHash),
+      List(Transaction("Hello Blockchain, this is Genesis :)")),
+      StdMiningTarget
+    )
+
+    mine(genesisTemplate)
   }
 }
